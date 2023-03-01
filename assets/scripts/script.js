50ab0c597be81d5a2c9e0ac40c99ee51581c2b40
Felipe-Belotto <felipeeduardo817@gmail.com>
2023-03-01 15:59:58 -0300

    Update script.js

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -57,26 +57,31 @@ function calculaDocumentacao() {
         break
     }
     
-   
-
     function calculaRegistro(valorCompra) {
-        /* Valores dos serviços cartórarios sem desconto, usando a tabela de SP como refêrencia */
-        if (valorCompra > 34260.01 && valorCompra < 102780) { return registro = 1699.18 }
-        else if (valorCompra > 102780.01 && valorCompra < 171300) { return registro = 1894.28 }
-        else if (valorCompra > 171300.01 && valorCompra < 205560) { return registro = 2326.54 }
-        else if (valorCompra > 205560.01 && valorCompra < 239820.01) { return registro = 2731.51 }
-        else if (valorCompra > 239820.01 && valorCompra < 274080.01) { return registro = 2946.72 }
-        else if (valorCompra > 274080.01 && valorCompra < 308340.01) { return registro = 3163.38 }
-        else if (valorCompra > 308340.01 && valorCompra < 342600.01) { return registro = 3325.82 }
-        else if (valorCompra > 342600.01 && valorCompra < 685200.01) { return registro = 3541.74 }
-        else if (valorCompra > 685200.01 && valorCompra < 1027800.01) { return registro = 4638.21 }
-        else if (valorCompra > 1027800.01 && valorCompra < 1370400.01) { return registro = 5441.15 }
-        else if (valorCompra > 1370400.01 && valorCompra < 1713000.01) { return registro = 6244.06 }
-        else if (valorCompra > 1713000.01 && valorCompra < 2055600.01) { return registro = 6659.19 }
-        else if (valorCompra > 2055600.01 && valorCompra < 3426000) { return registro = 8734.85 }
-        else { registro = "Compra e venda fora do limite da nossa programação" }
-    }
-
+        const valoresRegistro = [
+          { valorMin: 34260.01, valorMax: 102780, valor: 1699.18 },
+          { valorMin: 102780.01, valorMax: 171300, valor: 1894.28 },
+          { valorMin: 171300.01, valorMax: 205560, valor: 2326.54 },
+          { valorMin: 205560.01, valorMax: 239820.01, valor: 2731.51 },
+          { valorMin: 239820.01, valorMax: 274080.01, valor: 2946.72 },
+          { valorMin: 274080.01, valorMax: 308340.01, valor: 3163.38 },
+          { valorMin: 308340.01, valorMax: 342600.01, valor: 3325.82 },
+          { valorMin: 342600.01, valorMax: 685200.01, valor: 3541.74 },
+          { valorMin: 685200.01, valorMax: 1027800.01, valor: 4638.21 },
+          { valorMin: 1027800.01, valorMax: 1370400.01, valor: 5441.15 },
+          { valorMin: 1370400.01, valorMax: 1713000.01, valor: 6244.06 },
+          { valorMin: 1713000.01, valorMax: 2055600.01, valor: 6659.19 },
+          { valorMin: 2055600.01, valorMax: 3426000, valor: 8734.85 },
+        ];
+      
+        let Cartorio = valoresRegistro.find(valorCartorio => valorCompra > valorCartorio.valorMin && valorCompra < valorCartorio.valorMax);
+      
+        if (!Cartorio) {
+          return registro = "Compra e venda fora do limite da nossa programação";
+        }
+         return registro = Cartorio.valor;
+      }
+      
     function imprimeResultado() {
         botaoRecarregar.style.display = "block"
         resposta.style.display = "flex";
@@ -177,8 +182,6 @@ function calculaDocumentacao() {
         }
     }
 
-    
     calculaRegistro(valorCompra)
     imprimeResultado()
 }
-
