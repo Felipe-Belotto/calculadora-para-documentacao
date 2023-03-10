3a9094f8b7493e2ef822ab205fdc8faf8531e1ee
Felipe-Belotto <felipeeduardo817@gmail.com>
2023-03-10 17:00:33 -0300

    att

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,170 +1,170 @@
 class Documentacao {
-    
-    constructor(){
+
+    constructor() {
         this.valor = []
         this.cidadeValores = []
         this.agenciaValores = []
-        this.cartorioValores = [] 
+        this.cartorioValores = []
         this.enquadramentoValores = []
         this.documentacaoValores = []
     }
 
-    lerDados(){
-     this.valor.compra = document.getElementById('compra').valueAsNumber
-     this.valor.financiamento = document.getElementById('financiamento').valueAsNumber
-     this.valor.recursosProprios = (this.valor.compra) - (this.valor.financiamento)
-     this.valor.cidade = document.querySelector('#cidade').value
-     this.valor.banco = document.querySelector('#banco').value
-     this.valor.agencia = document.querySelector('#agencia').value
-     this.valor.enquadramento = document.querySelector('#enquadramento').value
+    lerDados() {
+        this.valor.compra = document.getElementById('compra').valueAsNumber
+        this.valor.financiamento = document.getElementById('financiamento').valueAsNumber
+        this.valor.recursosProprios = (this.valor.compra) - (this.valor.financiamento)
+        this.valor.cidade = document.querySelector('#cidade').value
+        this.valor.banco = document.querySelector('#banco').value
+        this.valor.agencia = document.querySelector('#agencia').value
+        this.valor.enquadramento = document.querySelector('#enquadramento').value
 
-     this.verificaCidade()
-     this.verificaAgencia()
-     this.verificaRegistro()
-     this.verificaEnquadramento()
-     this.verificaExecucoes()
+        this.verificaCidade()
+        this.verificaAgencia()
+        this.verificaRegistro()
+        this.verificaEnquadramento()
+        this.verificaExecucoes()
     }
 
-    verificaCidade(){
+    verificaCidade() {
 
-    switch(this.valor.cidade){
-    
-        case "campinas": 
-        this.cidadeValores.limiteFGTS = 264000
-        this.cidadeValores.itbi = this.valor.compra * 0.027
-        break
+        switch (this.valor.cidade) {
 
-        case "guarulhos": 
-        this.cidadeValores.limiteFGTS = 264000
-        this.cidadeValores.itbi = (0.02 * this.valor.recursosProprios) + (0.005 * this.valor.financiamento)
-        break
-    }
-    }
+            case "campinas":
+                this.cidadeValores.limiteFGTS = 264000
+                this.cidadeValores.itbi = this.valor.compra * 0.027
+                break
 
-    verificaAgencia(){
-
-    switch(this.valor.agencia){
-
-        case "0676":
-        this.agenciaValores.relacionamento = 1500  
-        break
-
-        case "3231":
-        this.agenciaValores.relacionamento = 800  
-        break 
-    }
-    }
-
-    verificaRegistro(){
-    this.lerDados
-    this.cartorioValores.referenciaCompra = [34260.01, 102780.01, 171300.01, 205560.01, 239820.01, 274080.01, 308340.01, 342600.01, 685200.01, 1027800.01, 1370400.01, 1713000.01, 2055600.01, Infinity]
-    this.cartorioValores.referenciaValor = [1699.18, 1894.28, 2326.54, 2731.51, 2946.72 , 3163.38 ,3325.82 , 3541.74, 4638.21, 5441.15, 6244.06, 6659.19, 8734.85]
-    
-    for(let i = 0; i < this.cartorioValores.referenciaCompra.length - 1; i++){
-        if(this.valor.compra > this.cartorioValores.referenciaCompra[i]  &&  this.valor.compra < this.cartorioValores.referenciaCompra[i + 1]){
-             this.cartorioValores.registro = this.cartorioValores.referenciaValor[i]
+            case "guarulhos":
+                this.cidadeValores.limiteFGTS = 264000
+                this.cidadeValores.itbi = (0.02 * this.valor.recursosProprios) + (0.005 * this.valor.financiamento)
+                break
         }
     }
+
+    verificaAgencia() {
+
+        switch (this.valor.agencia) {
+
+            case "0676":
+                this.agenciaValores.relacionamento = 1500
+                break
+
+            case "3231":
+                this.agenciaValores.relacionamento = 800
+                break
+        }
     }
 
-    verificaEnquadramento(){
+    verificaRegistro() {
+        this.lerDados
+        this.cartorioValores.referenciaCompra = [34260.01, 102780.01, 171300.01, 205560.01, 239820.01, 274080.01, 308340.01, 342600.01, 685200.01, 1027800.01, 1370400.01, 1713000.01, 2055600.01, Infinity]
+        this.cartorioValores.referenciaValor = [1699.18, 1894.28, 2326.54, 2731.51, 2946.72, 3163.38, 3325.82, 3541.74, 4638.21, 5441.15, 6244.06, 6659.19, 8734.85]
+
+        for (let i = 0; i < this.cartorioValores.referenciaCompra.length - 1; i++) {
+            if (this.valor.compra > this.cartorioValores.referenciaCompra[i] && this.valor.compra < this.cartorioValores.referenciaCompra[i + 1]) {
+                this.cartorioValores.registro = this.cartorioValores.referenciaValor[i]
+            }
+        }
+    }
+
+    verificaEnquadramento() {
 
         let telaEnquadramento = document.querySelector(".caixa_enquadramento")
         telaEnquadramento.classList.remove("display-none")
         console.log(this.valor.banco)
-        switch(this.valor.banco){
-        
-        case "caixa":
-        
-        switch(this.valor.enquadramento){
+        switch (this.valor.banco) {
 
-            case "mcmv": 
-            this.enquadramentoValores.taxa = (this.valor.financiamento * 0.015) + 50
-            break
+            case "caixa":
 
-            case "proCotista": 
-            this.enquadramentoValores.taxa = (this.valor.financiamento * 0.015) + 50
-            break
+                switch (this.valor.enquadramento) {
 
-            case "sbpe": 
-            this.enquadramentoValores.taxa = 1050
-            break 
+                    case "mcmv":
+                        this.enquadramentoValores.taxa = (this.valor.financiamento * 0.015) + 50
+                        break
+
+                    case "proCotista":
+                        this.enquadramentoValores.taxa = (this.valor.financiamento * 0.015) + 50
+                        break
+
+                    case "sbpe":
+                        this.enquadramentoValores.taxa = 1050
+                        break
+                }
+                break
+
+
+            case "bb":
+
+                switch (this.valor.enquadramento) {
+
+                    case "mcmv":
+                        this.enquadramentoValores.taxa = (this.valor.financiamento * 0.015) + 50
+                        break
+
+                    case "proCotista":
+                        this.enquadramentoValores.taxa = (this.valor.financiamento * 0.015) + 50
+                        break
+
+                    case "sbpe":
+                        this.enquadramentoValores.taxa = 1365
+                        break
+                }
+                break
+
+            case "itau":
+
+                telaEnquadramento.classList.add("display-none")
+                this.enquadramentoValores.taxa = 1950
+                break
         }
-        break
-        
+    }
 
-        case "bb":
-        
-        switch(this.valor.enquadramento){
-            
-            case "mcmv": 
-            this.enquadramentoValores.taxa = (this.valor.financiamento * 0.015) + 50
-            break
+    verificaExecucoes() {
 
-            case "proCotista": 
-            this.enquadramentoValores.taxa = (this.valor.financiamento * 0.015) + 50
-            break
+        this.documentacaoValores.taxa = this.enquadramentoValores.taxa
+        this.documentacaoValores.relacionamento = this.agenciaValores.relacionamento
+        this.documentacaoValores.itbi = this.cidadeValores.itbi
+        this.documentacaoValores.registro = this.cartorioValores.registro
 
-            case "sbpe": 
-            this.enquadramentoValores.taxa = 1365
-            break   
+        let resposta = window.document.getElementById('resposta')
+        let resultado = (valor) => { resposta.innerHTML = valor }
+        let modificaDinheiroReal = (valor) => { return valor.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }); }
+
+
+        function executaErro() {
+            resposta.classList.remove('caixa_reposta')
+            resposta.classList.add('resposta_erro')
         }
-        break
-        
-        case "itau":
-        
-        telaEnquadramento.classList.add("display-none")
-        this.enquadramentoValores.taxa = 1950
-        break
+
+        function validaErro() {
+            resposta.classList.remove('resposta_erro')
+            resposta.classList.add('caixa_reposta')
         }
-    }
 
-    verificaExecucoes(){
+        botaoCopiar.style.display = "block"
+        botaoRecarregar.style.display = "block"
+        resposta.style.display = "flex";
 
-    this.documentacaoValores.taxa = this.enquadramentoValores.taxa 
-    this.documentacaoValores.relacionamento = this.agenciaValores.relacionamento
-    this.documentacaoValores.itbi = this.cidadeValores.itbi
-    this.documentacaoValores.registro = this.cartorioValores.registro
+        if (this.valor.compra < 62000) {
+            executaErro()
+            resultado("Valor de compra e venda inferior ao minimo " + modificaDinheiroReal(62000))
+        }
 
-    let resposta = window.document.getElementById('resposta')
-    let resultado = (valor) => { resposta.innerHTML = valor}
-    let modificaDinheiroReal = (valor) => { return valor.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }); }
+        else if (this.valor.compra * 0.8 < this.valor.financiamento) {
+            executaErro()
+            resultado("Valor de financiamento maior do que 80% do valor de compra,<br> valor possivel: " + modificaDinheiroReal(this.valor.compra * 0.8))
+        }
+
+        else {
+            console.log(this.documentacaoValores)
+        }
 
 
-    function executaErro() {
-        resposta.classList.remove('caixa_reposta')
-        resposta.classList.add('resposta_erro')
-    }
-
-    function validaErro() {
-        resposta.classList.remove('resposta_erro')
-        resposta.classList.add('caixa_reposta')
-    }
-
-    botaoCopiar.style.display = "block"
-    botaoRecarregar.style.display = "block"
-    resposta.style.display = "flex";
-
-    if(this.valor.compra < 62000){
-        executaErro()
-        resultado("Valor de compra e venda inferior ao minimo " + modificaDinheiroReal(62000))
-    }
-
-    else if (this.valor.compra * 0.8 < this.valor.financiamento){
-        executaErro()
-        resultado("Valor de financiamento maior do que 80% do valor de compra,<br> valor possivel: " + modificaDinheiroReal(this.valor.compra * 0.8))
-    }
-
-    else {
-        console.log(this.documentacaoValores)
-    }
-
-    
     }
 
 
- }
+}
 
-   
 
- let documentacao = new Documentacao();
+
+let documentacao = new Documentacao();
